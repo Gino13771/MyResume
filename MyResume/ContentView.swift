2680c4d31a779b6c4d25bffb8cd6db0b42d6ad1b
Gino <gino13771@gmail.com>
2024-04-14 18:24:24 +0800

    no message

--- MyResume/ContentView.swift
+++ MyResume/ContentView.swift
@@ -83,7 +83,6 @@ struct ContentView: View {
         }
     }
     
-    
     var experienceView: some View {
         VStack (spacing: 0)  {
             ForEach(me.experiences.indices, id: \.self) { index in
