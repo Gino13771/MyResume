455f165a07450b807b055618cb5a3793a893a3a9
Gino <gino13771@gmail.com>
2024-04-24 20:21:02 +0800

    no message

--- MyResume/ContactView.swift
+++ MyResume/ContactView.swift
@@ -38,6 +38,7 @@ struct ContactView: View {
         .background(RoundedRectangle(cornerRadius: 20).foregroundColor(Color(uiColor: .tertiarySystemBackground)))
         .padding()
     }
+    
     func openUrl(_ url: String) {
         let url = URL(string: url)!
         
